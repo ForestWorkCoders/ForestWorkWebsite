8c154ef458249809ed33bc806970a5ac8c68ee54
EaglePB2 <eagleforceleader@hotmail.com>
2022-12-18 15:44:09 +0800

    Create counter.js

---
+++ assets/js/components/counter.js
@@ -0,0 +1,8 @@
+const counter = document.createElement('module');
+
+var then = new Date(2016, 09, 15), // month is zero based
+    now  = new Date;               // no arguments -> current date
+
+counter.innerHTML = `<span data-purecounter-start="0" data-purecounter-end="` + Math.round((now - then) / (1000 * 60 * 60 * 24)) + `" data-purecounter-duration="1" class="purecounter"></span>`
+
+document.getElementById("counter").appendChild(counter);
