9a9fe8303442bcb7f4818841833ce11e3a3f8076
EaglePB2 <eagleforceleader@hotmail.com>
2022-12-18 16:23:11 +0800

    Create member_count.js

---
+++ assets/js/components/member_count.js
@@ -0,0 +1,26 @@
+const counter = document.createElement('module');
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('https://discord.com/api/v9/invites/qKrub9b?with_counts=true',
+function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    counter.innerHTML = `<span data-purecounter-start="0" data-purecounter-end="` + data.approximate_member_count + `" data-purecounter-duration="1" class="purecounter"></span>`
+    document.getElementById("counter2").appendChild(counter);
+  }
+});
