2d4b042438eaeeb9fc982df440d52d6a4823acda
EaglePB2 <eagleforceleader@hotmail.com>
2022-12-18 19:27:11 +0800

    Update counter.js

--- assets/js/components/counter.js
+++ assets/js/components/counter.js
@@ -5,4 +5,4 @@ var then = new Date(2016, 09, 15), // month is zero based
 
 counter.innerHTML = `<span data-purecounter-start="0" data-purecounter-end="` + Math.round((now - then) / (1000 * 60 * 60 * 24)) + `" data-purecounter-duration="1" class="purecounter"></span>`
 
-document.getElementById("counter").appendChild(counter2);
+document.getElementById("alive_counter").appendChild(counter2);
