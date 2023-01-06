dd5fdcfc4f3f0e03b9a5d501997fbc78b2f89488
EaglePB2 <eagleforceleader@hotmail.com>
2023-01-06 12:56:45 +0800

    December Update!

--- pages/TOURNAMENT/Mahjong/pages/venue.js
+++ pages/TOURNAMENT/Mahjong/pages/venue.js
@@ -411,3 +411,44 @@ document.getElementById('iota').innerHTML = `
       </span>
     </div>
 `;
+
+document.getElementById('aesir').innerHTML = `
+    <span class="survey-country list-only">
+      DEC
+    </span>
+    
+    <span class="survey-name">
+      林間盃 十二月份 日麻積分賽
+    </span>
+    
+    <span class="survey-country grid-only">
+      DEC
+    </span>
+  
+    <div class="pull-right">
+    <span class="survey-progress">
+    <span class="survey-progress-bg">
+      <span class="survey-progress-fg" style="width: 250%;"></span>
+    </span>
+    
+    <span class="survey-progress-labels">
+      <span class="survey-progress-label">
+        100%
+      </span>
+
+      <span class="survey-completes">
+        12 / 12
+      </span>
+    </span>
+  </span>
+      <span class="survey-end-date">
+        2022 - DEC 30
+      </span>
+      <span class="survey-stage">
+        <span class="stage draft">Draft</span>
+        <span class="stage awarded">Awarded</span>
+        <span class="stage live">Live</span>
+        <span class="stage ended active">Ended</span>        
+      </span>
+    </div>
+`;
