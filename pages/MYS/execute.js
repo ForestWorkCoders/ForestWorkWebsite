2fddab4777fc68d8bf1e11ba2ebb4d30daedd568
EaglePB2 <eagleforceleader@hotmail.com>
2023-02-17 19:05:30 +0800

    Testing Discord bot works

---
+++ pages/MYS/execute.js
@@ -0,0 +1,15 @@
+const userId = "203029070693269504";
+const token = "DISCORD_TOKEN";
+
+fetch(`https://discord.com/api/users/${userId}`, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(`The username for user ${userId} is ${data.username}#${data.discriminator}`);
+  })
+  .catch(error => {
+    console.error(`Error fetching user information for user ${userId}: ${error}`);
+  });
