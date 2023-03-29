b0ab7c2b8c7827ca0033080238fbe9d748e9b5f5
EaglePB2 <eagleforceleader@hotmail.com>
2023-03-29 23:22:54 +0800

    March Mahjong —— Opened!

--- pages/MAHJONG/League/venue.js
+++ pages/MAHJONG/League/venue.js
@@ -534,3 +534,44 @@ document.getElementById('yodazo').innerHTML = `
       </span>
     </div>
 `;
+
+document.getElementById('mihayo').innerHTML = `
+    <span class="survey-country list-only">
+      MAR
+    </span>
+    
+    <span class="survey-name">
+      2023 林間盃 三月份 日麻積分賽
+    </span>
+    
+    <span class="survey-country grid-only">
+      MAR
+    </span>
+  
+    <div class="pull-right">
+    <span class="survey-progress">
+    <span class="survey-progress-bg">
+      <span class="survey-progress-fg" style="width: 250%;"></span>
+    </span>
+    
+    <span class="survey-progress-labels">
+      <span class="survey-progress-label">
+        100%
+      </span>
+
+      <span class="survey-completes">
+        16 / 16
+      </span>
+    </span>
+  </span>
+      <span class="survey-end-date">
+        2023 - MAR 29
+      </span>
+      <span class="survey-stage">
+        <span class="stage draft">Draft</span>
+        <span class="stage awarded">Awarded</span>
+        <span class="stage live">Live</span>
+        <span class="stage ended active">Ended</span>        
+      </span>
+    </div>
+`;
