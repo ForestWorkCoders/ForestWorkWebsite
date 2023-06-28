61c9c2327c250fabbeacd96423c00afc4012837e
EaglePB2 <46810904+eaglePB2@users.noreply.github.com>
2023-06-28 22:31:56 +0800

    Updated June

--- pages/MAHJONG/League/venue.js
+++ pages/MAHJONG/League/venue.js
@@ -658,3 +658,44 @@ document.getElementById('May2023').innerHTML = `
       </span>
     </div>
 `;
+
+document.getElementById('Jun2023').innerHTML = `
+    <span class="survey-country list-only">
+      JUN
+    </span>
+    
+    <span class="survey-name">
+      2023 林間盃 六月份 日麻積分賽
+    </span>
+    
+    <span class="survey-country grid-only">
+      JUN
+    </span>
+  
+    <div class="pull-right">
+    <span class="survey-progress">
+    <span class="survey-progress-bg">
+      <span class="survey-progress-fg" style="width: 250%;"></span>
+    </span>
+    
+    <span class="survey-progress-labels">
+      <span class="survey-progress-label">
+        100%
+      </span>
+
+      <span class="survey-completes">
+        16 / 16
+      </span>
+    </span>
+  </span>
+      <span class="survey-end-date">
+        2023 - JUN 28
+      </span>
+      <span class="survey-stage">
+        <span class="stage draft">Draft</span>
+        <span class="stage awarded">Awarded</span>
+        <span class="stage live">Live</span>
+        <span class="stage ended active">Ended</span>        
+      </span>
+    </div>
+`;
