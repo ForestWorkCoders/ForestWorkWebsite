4df323d0f56f3fc4d2b1b0b11608447c894d8860
EaglePB2 <46810904+eaglePB2@users.noreply.github.com>
2023-09-27 23:31:53 +0800

    Quick Fix #2

--- pages/OTHERS/Quotes/js/execute.js
+++ pages/OTHERS/Quotes/js/execute.js
@@ -8,12 +8,8 @@ $(document).ready(function () {
             const messages = data[i].messages;
 
             // Generate message HTML
-            let messagesHTML = '';
             for (let j = 0; j < messages.length; j++) {
-                messagesHTML += `<p class="card-text">${messages[j]}</p>`;
-            }
-
-            html += `<div class="row">
+                html += `<div class="row">
                         <div class="col-lg-4 col-md-6 mb-3">
                             <div class="card border border-secondary shadow-0 " style="background-color:#fefefe;">
                                 <div class="card-body">
@@ -22,7 +18,7 @@ $(document).ready(function () {
                                             <img class="" src="${image}" alt="${username}" width="128px">
                                         </div>
                                         <div class="col-lg-8 col-md-6 col-sm-8">
-                                            ${messagesHTML}
+                                            <p class="card-text">${messages[j]}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -30,17 +26,18 @@ $(document).ready(function () {
                             </div>
                         </div>
                     </div>`;
+            }
+        }
+            html += `<hr>`
+            return html;
         }
 
-        return html;
-    }
-
-    // Fetch the JSON data and populate the container with the generated cards
-    fetch('pages/OTHERS/Quotes/json/quote.json')
-        .then(response => response.json())
-        .then(data => {
-            const quotesData = data;
-            $('#quoteContainer').html(generateCards(quotesData));
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
-});
+        // Fetch the JSON data and populate the container with the generated cards
+        fetch('pages/OTHERS/Quotes/json/quote.json')
+            .then(response => response.json())
+            .then(data => {
+                const quotesData = data;
+                $('#quoteContainer').html(generateCards(quotesData));
+            })
+            .catch(error => console.error('Error fetching JSON:', error));
+    });
