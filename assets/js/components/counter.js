09e68bc5d7c0da92991dace061aa273e8efc7e88
EaglePB2 <eagleforceleader@hotmail.com>
2023-10-22 13:35:56 +0800

    Create Branch for Version 1 Webpage

--- assets/js/components/counter.js
+++
@@ -1,8 +0,0 @@
-const counter2 = document.createElement('module');
-
-var then = new Date(2016, 09, 15), // month is zero based
-    now  = new Date;               // no arguments -> current date
-
-counter2.innerHTML = `<span data-purecounter-start="0" data-purecounter-end="` + Math.round((now - then) / (1000 * 60 * 60 * 24)) + `" data-purecounter-duration="1" class="purecounter"></span>`
-
-document.getElementById("alive_counter").appendChild(counter2);
