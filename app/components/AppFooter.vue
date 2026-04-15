fe3f866507005e53a85c54417230459b14aeb340
EaglePB2 <eagleforceleader@hotmail.com>
2026-04-15 16:34:20 +0800

    Add a Footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,6 +1,38 @@
 <template>
+  <UFooter>
+    <template #left>
+      <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }}. All rights reserved.</p>
+    </template>
 
+    <template #right>
+      <UButton
+        icon="i-simple-icons-discord"
+        color="neutral"
+        variant="ghost"
+        to="https://discord.gg/XhAHbCq3VB"
+        target="_blank"
+        aria-label="Discord"
+      />
+      <UButton
+        icon="i-simple-icons-youtube"
+        color="neutral"
+        variant="ghost"
+        to="https://www.youtube.com/@forestwork"
+        target="_blank"
+        aria-label="Youtube"
+      />
+      <UButton
+        icon="i-simple-icons-X"
+        color="neutral"
+        variant="ghost"
+        to="https://twitter.com/forestwork"
+        target="_blank"
+        aria-label="X"
+      />
+    </template>
+  </UFooter>
 </template>
+
 
 <script>
 export default {
