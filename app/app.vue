b98a96df852e59fafe28c8ab92bb3cf114848c67
EaglePB2 <eagleforceleader@hotmail.com>
2026-04-16 16:16:24 +0800

    Installation of Speed insights

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,10 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <SpeedInsights />
   </div>
 </template>
+
+<script setup>
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
