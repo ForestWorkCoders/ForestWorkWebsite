c931bd4f15b674fc74f63b5be4c45bbbf3e73ada
EaglePB2 <eagleforceleader@hotmail.com>
2026-05-07 14:13:27 +0800

    SEO Improvement: Add the SEO Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   // Nuxt 4 的兼容性日期设定，确保你使用最新的内部机制
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@vercel/speed-insights/nuxt', '@vercel/analytics/nuxt', '@vite-pwa/nuxt'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@vercel/speed-insights/nuxt', '@vercel/analytics/nuxt'],
   css: ['~/assets/css/main.css'],
 
   supabase: {
@@ -19,32 +19,6 @@ export default defineNuxtConfig({
     }
   },
 
-  pwa: {
-    manifest: {
-      "name": "林間小鎮 ForestWork",
-      "short_name": "林間",
-      "description": "一個賽事戰報歷史游覽網頁",
-      "start_url": "/",
-      "display": "browser",
-      "theme_color": "#4C7766",
-      "background_color": "#ffffff",
-      "icons": [
-        {
-          "src": "/icon-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "any maskable"
-        },
-        {
-          "src": "/icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "any maskable"
-        }
-      ]
-    }
-  },
-
   app: {
     head: {
       htmlAttrs: {
@@ -56,14 +30,14 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: '林间小镇是一个成立于2019年，有来自不同国家的热血玩家组成的社群。这里有好喝的玫瑰茶 (*^▽^*)' },
         { name: 'author', content: 'EaglePB2' },
-        { name: 'theme-color', content: '#ffffff' },
+        { name: 'theme-color', content: '#4C7766' },
         { name: 'keywords', content: '林间, 林间小镇, ForestWork' },
 
         // Open Graph 标签 (给 Facebook/Discord 预览用的)
         { property: 'og:type', content: 'website' },
         { property: 'og:title', content: '林间小镇 ForestWork' },
         { property: 'og:url', content: 'https://forestwork.vercel.app/' },
-        { property: 'og:image', content: 'https://og-image.xyz/og/林间小镇/ForestWork/forestwork.team/https/menlo/virtualshapes/ffffff/data.png' },
+        { property: 'og:image', content: 'https://og-image.xyz/og/Forestwork/A%20tournament%20result%20website/forestwork.team/https/menlo/virtualshapes/4c7766/data.png' },
         { property: 'og:description', content: '林间小镇是一个成立于2019年，有来自不同国家的热血玩家组成的社群。这里有好喝的玫瑰茶 (*^▽^*)' },
 
         // Twitter 标签
@@ -71,15 +45,13 @@ export default defineNuxtConfig({
         { name: 'twitter:title', content: '林间小镇 ForestWork' },
         { name: 'twitter:site', content: '@TeamForestWork' },
         { name: 'twitter:description', content: '林间小镇是一个成立于2019年，有来自不同国家的热血玩家组成的社群。这里有好喝的玫瑰茶 (*^▽^*)' },
-        { name: 'twitter:image', content: 'https://pbs.twimg.com/profile_images/1393930243920957442/6He2XdiU.jpg' },
+        { name: 'twitter:image', content: 'https://og-image.xyz/og/Forestwork/A%20tournament%20result%20website/forestwork.team/https/menlo/virtualshapes/4c7766/data.png' },
         { name: 'twitter:image:alt', content: 'ForestWork Logo' }
       ],
       link: [
-        // 注意这里的路径！把你的图标放到 public/ 目录下，路径直接写 /
-        { rel: 'shortcut icon', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon.png' },
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' }, 
+        { rel: 'icon', href: '/android-chrome-512x512.png', type: 'image/png' }, // 现代浏览器优先抓取这个
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }, // iOS 看到这个就够了
         { rel: 'manifest', href: '/site.webmanifest' }
       ],
       script: [
